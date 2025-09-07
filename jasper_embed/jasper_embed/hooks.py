9b44126d0a57acbee408f0b41a75870566345976
jamunachi <83000184+jamunachi@users.noreply.github.com>
2025-09-07 17:12:05 +0300

    Update hooks.py

--- jasper_embed/jasper_embed/hooks.py
+++ jasper_embed/jasper_embed/hooks.py
@@ -1,10 +1,8 @@
-﻿app_name = "jasper_embed"
+app_name = "jasper_embed"
 app_title = "Jasper Embed"
-app_publisher = "Your Company"
-app_description = "Embed JasperReports Server outputs inside ERPNext/Frappe"
-app_email = "support@example.com"
-app_version = "0.2.0"
+app_publisher = "jamunachi"
+app_description = "Embed JasperReports Server reports inside Frappe/ERPNext."
+app_email = "Neotec"
 app_license = "MIT"
 
-# Inject buttons globally on forms that have mappings
-app_include_js = ["jasper_embed/public/js/jasper_button.js"]
+# leave other hooks empty for now; add as needed later
